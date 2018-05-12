0d35125ddbde59743eedd9077f29ff6330bb430f
kevin <kevin@126.com>
2018-05-12 10:32:14 +0800

    create theme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mkdocs-itimor
+
+## 自定义的 mkdocs 主题
+
+[demo](https://itimor.github.io/django-oms/)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.10.24'
+
+
+setup(
+    name="mkdocs-itimor",
+    version=VERSION,
+    url='https://github.com/itimor/mkdocs-itimor',
+    license='MIT',
+    description='sexy mkdocs theme',
+    author='itimor',
+    author_email='itimor@126.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'itimor = itimor',
+        ]
+    },
+    zip_safe=False
+)
